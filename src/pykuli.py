d7fa0fa016ab532723fa258bf607916f9ae22c52
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2018-06-09 08:40:20 -0300

    Add move method (close issue #5)

--- src/pykuli.py
+++ src/pykuli.py
@@ -2,12 +2,20 @@
 # -*- encoding: utf-8 -*-
 import time
 
+from pykeyboard import PyKeyboard
+from pymouse import PyMouse
+
 
 class Pykuli(object):
 
     def __init__(self, default_path=u''):
+        self.mouse = PyMouse()
+        self.keyboard = PyKeyboard()
         self.default_path = default_path
 
     @staticmethod
     def wait(seconds):
         time.sleep(seconds)
+
+    def move(self, x, y):
+        self.mouse.move(x, y)
