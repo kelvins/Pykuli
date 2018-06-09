9059e98477e0c64fa9ab159a80d887d19beca66c
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2018-06-09 09:14:09 -0300

    pykuli: improve screenshot method

--- src/pykuli.py
+++ src/pykuli.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-import os
 import time
 
 from pykeyboard import PyKeyboard
@@ -11,15 +10,10 @@ from PIL import Image
 
 class Pykuli(object):
 
-    SCREENSHOT_NAME = u'monitor-1.png'
-
     def __init__(self, default_path=u''):
         self.mouse = PyMouse()
         self.keyboard = PyKeyboard()
         self.default_path = default_path
-
-    def __del__(self):
-        os.remove(u'monitor-1.png')
 
     @staticmethod
     def wait(seconds):
@@ -31,10 +25,20 @@ class Pykuli(object):
     def _mouse_click(self, x, y):
         self.mouse.click(x, y)
 
-    def _take_screenshot(self):
-        with mss() as screen:
-            screen.shot()
-        return self._load_image(self.SCREENSHOT_NAME)
+    @staticmethod
+    def take_screenshot():
+        with mss.mss() as sct:
+            # Get a screenshot of the 1st monitor
+            sct_img = sct.grab(sct.monitors[1])
 
-    def _load_image(self, image_path):
-        return Image.open(image_path)
+            # Create an Image
+            img = Image.new('RGB', sct_img.size)
+
+            # Best solution: create a list(tuple(R, G, B), ...) for putdata()
+            pixels = zip(sct_img.raw[2::4],
+                         sct_img.raw[1::4],
+                         sct_img.raw[0::4])
+
+            img.putdata(list(pixels))
+
+            return img
